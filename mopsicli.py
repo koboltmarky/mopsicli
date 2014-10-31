a5f74922f73a8acce06d1fe817bdd7d511dc449b
Marc Zimmermann <marc.zimmermann@mmbash.de>
2014-11-01 00:38:01 +0100

    add changeEnv and scale function. Remove all the urlib2 stuff

--- mopsicli.py
+++ mopsicli.py
@@ -1,14 +1,12 @@
 import click
-import urllib
-import urllib2
 import json
 import sys
 import requests
 
-#base_url = "http://mesosmaster02:8080"
-#api_version = "/v2/"
-base_url = "http://127.0.0.1:3000"
-api_version = "/v1/"
+#BASEURL = 'http://mesosmaster02:8080'
+#APIVERSION = '/v2/'
+BASEURL = 'http://127.0.0.1:3000'
+APIVERSION = '/v1/'
 
 @click.group()
 @click.version_option()
@@ -23,10 +21,52 @@ def marathon():
 def marathon_listapps():
 	"""List running apps"""
 
-	response = urllib2.urlopen(base_url+api_version+"apps")
-	data = json.load(response)   
- 	for each in data["apps"]:
- 		print each["id"][1:]
+	try:
+		r = requests.get(BASEURL+APIVERSION+'apps')
+		data = r.json()   
+		for each in data["apps"]:
+			print each["id"][1:]
+	
+	except requests.exceptions.RequestException as e:
+		print 'HTTP error: ',e 
+
+	except:
+		print 'Unexpected error:',sys.exc_info()[0]  
+		raise
+
+@marathon.command('getinfosapp')
+@click.argument('name')
+@click.option('--fields', nargs=1)
+@click.option('--listfields', is_flag=True)
+def marathon_getinfoapp(name,fields,listfields):
+	"""Get info of a running app"""
+
+	try:
+		r = requests.get(BASEURL+APIVERSION+'apps/'+name)
+		data = r.json()   
+		if listfields:
+			try:
+				print 'App as the following variables: \n'
+				for each in data["app"]:
+					print each
+			except: 
+				print 'Unexpected error:',sys.exc_info()[0] 
+		elif fields:
+			try:
+				for each in data["app"]:
+					if each == fields:
+						print "%s : %s" % (each, data["app"][fields])
+			except:
+				print 'Value %s not in json array ' % fields				
+		else:
+			print json.dumps(data,indent=4)
+	
+	except requests.exceptions.RequestException as e:
+		print 'HTTP error: ',e 
+
+	except:
+		print 'Unexpected error:',sys.exc_info()[0]  
+		raise
 
 @marathon.command('kill')
 @click.argument('name')
@@ -34,19 +74,19 @@ def marathon_killapp(name):
 	"""Kills a running app"""
 	
 	try:
-		url = base_url+api_version+"apps/"+name
+		url = BASEURL+APIVERSION+'apps/'+name
 		r = requests.delete(url)
 		
 		if r.status_code == 200:
 			print "Delete app %s succesful" % name
 		else:
-			print "Cannot delete app %s" % name 
+			print 'Cannot delete app %s' % name 
 
 	except requests.exceptions.RequestException as e:
-		print "HTTP error: "+str(e.code)+" "+e.reason 
+		print 'HTTP error: ',e 
 
 	except:
-		print "Unexpected error:",sys.exc_info()[0]  
+		print 'Unexpected error:',sys.exc_info()[0]  
 		raise
 
 @marathon.command('start')
@@ -54,39 +94,54 @@ def marathon_killapp(name):
 def marathon_startapp(filename):
 	"""Start a app. Reads input from jsonfile."""
 	try:
-		file_data = open(filename)
-		url =  base_url+api_version+"apps" 
-		payload = json.load(file_data)
+		fileData = open(filename)
+		url =  BASEURL+APIVERSION+'apps' 
+		payload = json.load(fileData)
 		headers = {'content-type':'application/json'}
 		r = requests.post(url,data=json.dumps(payload),headers=headers)
 
-		# req = urllib2.Request(url)
-		#req.add_header('Content-Type', 'application/json')
-		#response = urllib2.urlopen(req,json.dumps(data))
-
 	except requests.exceptions.RequestException as e:
-		print "HTTP error: "+str(e.code)+" "+e.reason 
+		print 'HTTP error: ',e 
 
 	except:
-		print "Unexpected error:",sys.exc_info()[0]  
+		print 'Unexpected error:',sys.exc_info()[0]  
 		raise
 
 @marathon.command('changeEnv')
 @click.argument('name')
 @click.argument('filename')
-def marathon_startapp(filename):
+def marathon_changeapp(name,filename):
 	"""Change settings of an app. Read changes from a jsonfile."""
  
 	try:
-		url = base_url+api_version+"apps/"+name
-		file_data = open(filename)
-		payload = json.load(file_data)
+		url = BASEURL+APIVERSION+'apps/'+name
+		fileData = open(filename)
+		payload = json.load(fileData)
 		headers = {'content-type':'application/json'}
 		r = requests.put(url,data=json.dumps(payload),headers=headers)
 
 	except requests.exceptions.RequestException as e:
-		print "HTTP error: "+str(e.code)+" "+e.reason 
+		print 'HTTP error: ',e 
 
 	except:
-		print "Unexpected error:",sys.exc_info()[0]  
+		print 'Unexpected error:',sys.exc_info()[0]  
+		raise
+
+@marathon.command('scale')
+@click.argument('name')
+@click.argument('count')
+def marathon_scaleapp(name,count):
+	"""Scale an running app."""
+ 
+	try:
+		url = BASEURL+APIVERSION+'apps/'+name
+		payload = {"instances" : count }
+		headers = {'content-type':'application/json'}
+		r = requests.put(url,data=json.dumps(payload),headers=headers)
+
+	except requests.exceptions.RequestException as e:
+		print 'HTTP error: ', e 
+
+	except:
+		print 'Unexpected error:', sys.exc_info()[0]  
 		raise
